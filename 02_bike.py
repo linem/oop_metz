5efbdfb564753b2b22449339e1c1c0af7fc9b73f
linemoller <line.moller@ess.eu>
2024-12-24 11:42:43 +0100

    2.3 add gear_inches

--- 02_bike.py
+++ 02_bike.py
@@ -1,10 +1,15 @@
 class Gear:
-    def __init__(self, chainring, cog):
+    def __init__(self, chainring, cog, rim, tire):
         self.chainring = chainring
         self.cog = cog
+        self.rim = rim
+        self.tire = tire
 
     def ratio(self):
         return self.chainring / float(self.cog)
 
-print(Gear(52, 11).ratio())
-print(Gear(30, 27).ratio())
+    def gear_inches(self):
+        return self.ratio() * (self.rim + (self.tire * 2))
+
+print(Gear(52, 11, 26, 1.5).gear_inches())
+print(Gear(52, 11, 24, 1.25).gear_inches())
