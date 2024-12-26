6c8d7da4b367ff4cdd340c0b5cad61de163418cc
linemoller <line.moller@ess.eu>
2024-12-26 17:50:42 +0100

    2.18 use inner Wheel class to calculate diameter

--- 02_bike.py
+++ 02_bike.py
@@ -2,17 +2,22 @@ class Gear:
     def __init__(self, chainring, cog, rim, tire):
         self.chainring = chainring
         self.cog = cog
-        self.rim = rim
-        self.tire = tire
+        self.wheel = self.Wheel(rim, tire)
 
     def ratio(self):
         return self.chainring / float(self.cog)
 
-    def diameter(self):
-        return self.rim + (self.tire * 2)
-
     def gear_inches(self):
-        return self.ratio() * self.diameter()
+        return self.ratio() * self.wheel.diameter()
+
+    class Wheel:
+        def __init__(self, rim, tire):
+            self.rim = rim
+            self.tire = tire
+
+        def diameter(self):
+            return self.rim + (self.tire * 2)
+
 
 print(Gear(52, 11, 26, 1.5).gear_inches())
 print(Gear(52, 11, 24, 1.25).gear_inches())
@@ -25,15 +30,6 @@ class RevealingReferences:
     def diameters(self):
         for wheel in self.wheels:
             print("wheel diameter:", self.diameter(wheel))
-
-    def diameter(self, wheel):
-        return wheel.rim + (wheel.tire * 2)
-
-
-    class Wheel:
-        def __init__(self, rim, tire):
-            self.rim = rim
-            self.tire = tire
 
     def wheelify(self, data):
         return [self.Wheel(cell[0], cell[1]) for cell in data]
