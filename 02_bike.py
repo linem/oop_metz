36cc5cd6bf17e11e63727a4be0f4a554c657e079
linemoller <line.moller@ess.eu>
2024-12-26 11:24:48 +0100

    2.11 depending on complicated data structures

--- 02_bike.py
+++ 02_bike.py
@@ -11,5 +11,21 @@ class Gear:
     def gear_inches(self):
         return self.ratio() * (self.rim + (self.tire * 2))
 
-print(Gear(52, 11, 26, 1.5).gear_inches())
-print(Gear(52, 11, 24, 1.25).gear_inches())
+# print(Gear(52, 11, 26, 1.5).gear_inches())
+# print(Gear(52, 11, 24, 1.25).gear_inches())
+
+
+class ObscuringReferences:
+    def __init__(self, data):
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
+
+    def diameters(self):
+        for wheel in self.data:
+            print("wheel diameter:", wheel[0] + (wheel[1] * 2))
+
+obj = ObscuringReferences([[622, 20], [622, 23], [559, 30], [559, 40]])
+obj.diameters()
