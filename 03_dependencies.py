ab4e597a46b886eee81de0378a6c701de1b97e1e
linemoller <line.moller@ess.eu>
2025-01-03 11:06:13 +0100

    3.16 reversing dependency from Gear depending on Wheel to Wheel depending on Gear

--- 03_dependencies.py
+++ 03_dependencies.py
@@ -1,22 +1,31 @@
-from external_gear_module import Gear
+class Gear:
+    def __init__(self, chainring, cog):
+        self.chainring = chainring
+        self.cog = cog
 
+    def gear_inches(self, diameter):
+        return self.ratio() * diameter
 
-class GearWrapper:
-    def __init__(self, chainring=None, cog=None, wheel=None):
-        self.gear = Gear(chainring, cog, wheel)
+    def ratio(self):
+        return self.chainring / float(self.cog)
 
 
 class Wheel:
-    def __init__(self, rim, tire):
+    def __init__(self, rim, tire, chainring, cog):
         self.rim = rim
         self.tire = tire
+        self.gear = Gear(chainring, cog)
 
     def diameter(self):
         return self.rim + (self.tire * 2)
 
+    def gear_inches(self):
+        return self.gear.gear_inches(self.diameter())
 
-print(GearWrapper(
-    wheel=Wheel(26, 1.5),
+
+print(Wheel(
+    rim=26,
+    tire=1.5,
     chainring=52,
-    cog=11).gear.gear_inches()
+    cog=11).gear_inches()
       )
